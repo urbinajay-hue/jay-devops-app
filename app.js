3a178c4f752a79354b6bf663ab8cfb25243d8f2d
Jay Urbina <urbina.jay@gmail.com>
2026-04-11 20:46:02 +0000

    Initial commit for DevOps app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+
+
+const PORT = 3000;
+
+
+
+app.get('/', (req, res) => {
+
+  res.send('Hello from Jay DevOps App!');
+
+});
+
+
+
+app.get('/health', (req, res) => {
+
+  res.status(200).send('OK');
+
+});
+
+
+
+app.listen(PORT, () => {
+
+  console.log(`App running on port ${PORT}`);
+
+});
